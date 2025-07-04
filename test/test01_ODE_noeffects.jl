6e77094202bdf184d5504ebb1bc2efb7ae8e065e
SimonHansul <simonhansul@gmail.com>
2025-07-04 12:38:24 +0200

    updated tests

--- test/test01_ODE_noeffects.jl
+++ test/test01_ODE_noeffects.jl
@@ -18,10 +18,114 @@ using StatsPlots
 using DataFrames, DataFramesMeta
 using StatsBase
 
-begin # embryo
-    p = deepcopy(Model1.params)
-    sim_emb, u0lrv, p_ind = Model1.sim_embryo(p)
-    @test abs(sim_emb.X_emb[end]) ≈ 0 atol = 1e-3
+using Revise
+
+@time import AmphiDEB: defaultparams, ODE_simulator
+using AmphiDEB
+using EcotoxSystems
+
+#using BenchmarkTools
+#AmphiDEB.ODE_simulator(AmphiDEB.defaultparams);
+#@benchmark AmphiDEB.ODE_simulator(AmphiDEB.defaultparams)
+#
+#VSCodeServer.@profview_allocs AmphiDEB.ODE_simulator(AmphiDEB.defaultparams)
+
+
+function run_basetest(m; pmod = p->p)
+
+    global p = deepcopy(defaultparams)
+
+    p.glb.t_max = 365*2
+    p.glb.pathogen_inoculation_time = Inf
+    p.glb.dX_in = [20., 20.]
+    p.spc.H_p = 55.
+
+    S_max_anl = AmphiDEB.calc_S_max_juv(p.spc)
+    H_max_anl_juv = AmphiDEB.calc_H_eq_juv(p.spc)
+
+    p = pmod(p) # optional modification of parameters
+
+    @time global sim = AmphiDEB.ODE_simulator(
+            p, 
+            reltol = 1e-10,
+            model = m,
+            saveat = 1/24, # we need high-resolution output to verify the solution
+            );
+
+    sim[!,:E_mt_rel] = sim.E_mt ./ (sim.S + sim.E_mt)
+    sim[!,:dI] = vcat(0, diff(sim.I))
+    sim[!,:dI_rel] = sim.dI ./ (sim.S .^(2/3))
+    sim[!,:W_tot] = sim.S .+ sim.E_mt 
+    
+    plt = plot_statevars(
+        @subset(sim, :t .< 40), 
+        [:S, :H, :E_mt, :R, :X_emb, :J, :dI_rel, :W_tot, :f_X, :embryo, :larva, :metamorph], 
+        xrotation = 45
+        )
+    hline!([p.spc.H_j1], subplot=2, color = :gray, linestyle = :dash)
+
+    display(plt)
+
+    # check final structural mass
+    
+    @test isapprox(S_max_anl, maximum(sim.S), atol = 5) 
+
+    # check that all life stage indicators max out close to 1
+    
+    @test 0.99 < maximum(sim.embryo) < 1.01
+    @test 0.99 < maximum(sim.larva) < 1.01
+    @test 0.99 < maximum(sim.metamorph) < 1.01
+    @test 0.99 < maximum(sim.juvenile) < 1.01
+    @test 0.99 < maximum(sim.adult) < 1.01
+
+    # check that the sum of life stage indicators is always approximately 1
+
+    sum_indicators = @. sim.embryo + sim.larva + sim.metamorph + sim.juvenile + sim.adult
+
+    @test unique(isapprox.(1, sum_indicators, atol = 1e-3)) == [true]
+    
+    # verify that the size-specific maintenance rate is approximately constant by back-calculating k_M from the state variables
+        
+    sim[!,:dM] = vcat(0, diff(sim.M)) ./ vcat(0, diff(sim.t))
+    k_M = sim.dM ./ sim.S 
+
+    reldiff_kM = begin
+        kmin, kmax = extrema(k_M[50:end]) # we allow for a small "burn in" period: at the beginning, k_M is close to 0 and the relative error will be large 
+        kmax / kmin
+    end
+
+    @test 0.9 < reldiff_kM < 1.1
+
+    # same for k_J
+
+    sim[!,:dH] = vcat(0, diff(sim.J)) ./ vcat(0, diff(sim.t))
+    k_J = sim.dH ./ sim.H 
+
+    reldiff_kJ = begin
+        kmin, kmax = extrema(k_J[50:end]) 
+        kmax / kmin
+    end
+
+    @test 0.9 < reldiff_kJ < 1.1
+
+    # comparing analytically calculated with simulated equilibrium maturity 
+    # for this we need to disengage H_p and re-run the simulation
+
+    p.glb.t_max = 365*10
+    p.spc.H_p = Inf
+    
+    @time global sim = AmphiDEB.ODE_simulator(
+            p, 
+            saveat = 1/24, # we need high-resolution output to verify the solution
+            model = m
+            );
+
+    @info "
+    Analytically caluclated equilibrium maturity: $(round(H_max_anl_juv, sigdigits = 3))
+    Simulated maximum maturity: $(round(maximum(sim.H)))
+    "
+
+    @test 0.8*H_max_anl_juv <= maximum(sim.H) <= 1.2*H_max_anl_juv 
 end
 
 begin # larva
@@ -29,11 +133,16 @@ begin # larva
     @test sim_lrv.H[end] ≈ p.spc.H_j1    
 end
 
-begin # metamorph
-    sim_mt, u0juv, p_ind = Model1.sim_metamorph(p_ind, u0mt)
-    @df sim_mt plot(:t, [:S :E_mt :I], layout = (1,3), marker = true, leg = false)
-    @test sim_mt.E_mt[end] ≈ 0. atol = 1e-3
-end
+#
+#@testset "Default parameters M2" begin
+#    run_basetest(
+#        AmphiDEB.M2_complete_ODE_with_loglogistic_TD!; 
+#        pmod = p -> begin 
+#            p.spc.delta_k_M_mt = 0.33; 
+#            return p 
+#        end
+#        )
+#end
 
 begin # juvenile 
     sim_juv, u0ad, p_ind = Model1.sim_juvenile(p_ind, u0juv)
