ff41855db47a0581cde739593696334e79bb27ee
Simon Hansul <hansul@gaiac-eco.de>
2026-05-03 18:52:43 +0200

    update deps

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,24 +1,33 @@
-@time begin 
+if !isinteractive()
     using Pkg; Pkg.activate("test")
-
-    using Test
-    using Plots, StatsPlots, Plots.Measures
-    using Distributions
-    using OrdinaryDiffEq
-    default(leg = false, lw = 1.5)
-
-    using Chain
-    using DataFrames, DataFramesMeta
-    using StatsBase
-
-    using Revise
-
-    using AmphiDEB
-    using EcotoxSystems
-
-    import EcotoxSystems: sig
-    import EcotoxSystems: constrmvec
 end
+
+if isinteractive()
+    using Pkg; Pkg.activate("lib/AmphiDEB.jl/test")
+end
+
+using Pkg; Pkg.develop(path="lib/AmphiDEB.jl")
+
+Pkg.instantiate()
+Pkg.resolve()
+
+using Test
+using Plots, StatsPlots, Plots.Measures
+using Distributions
+using OrdinaryDiffEq
+default(leg = false, lw = 1.5)
+
+using Chain
+using DataFrames, DataFramesMeta
+using StatsBase
+
+using Revise
+
+using AmphiDEB
+using EcotoxSystems
+
+import EcotoxSystems: sig
+import EcotoxSystems: constrmvec
 
 include("test01_ODE_noeffects.jl")
 include("test02_IBM_noeffects.jl")
